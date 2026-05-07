a62eb241d305046415dbcab7b09569298542c7ae
magnimel <65434087+magnimel@users.noreply.github.com>
2026-05-07 03:30:40 -0700

    feat: update sphere constructor to include material parameter and adjust hit method

--- include/sphere.hpp
+++ include/sphere.hpp
@@ -2,10 +2,12 @@
 #define SPHERE_H
 
 #include "hittable.hpp"
+#include "material.hpp"
 
 class sphere : public hittable {
 public:
-    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+    sphere(const point3& center, double radius, shared_ptr<material> mat) 
+    : center(center), radius(std::fmax(0, radius)), mat(mat) {}
     
     bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
         vec3 oc = center - r.origin();
@@ -27,6 +29,7 @@ public:
         }
 
         rec.t = root;
+        rec.mat = mat;
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
@@ -38,6 +41,7 @@ public:
 private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 #endif
