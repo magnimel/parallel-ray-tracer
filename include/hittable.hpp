3ff3b78f4d4601508662cb0dc0edb4daa8b0d9fb
magnimel <65434087+magnimel@users.noreply.github.com>
2026-05-07 03:27:11 -0700

    feat: add material class with lambertian and metal implementations for scattering

--- include/hittable.hpp
+++ include/hittable.hpp
@@ -1,18 +1,21 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
+
 #include "vec3.hpp"
 #include "ray.hpp"
+
+class material;
 
 class hit_record {
     public:
         point3 p;
         vec3 normal;
         double t;
+        shared_ptr<material> mat;
         bool face_front;
 
         void set_face_normal(const ray& r, const vec3& outward_normal) {
-            // outward_normal is unit vector
             face_front = dot(r.direction(), outward_normal) < 0.0;
             normal = face_front ? outward_normal : -outward_normal;
         }
