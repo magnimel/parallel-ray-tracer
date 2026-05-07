787bcd645195c3ee834113b297f5b56d36d6e2f7
magnimel <65434087+magnimel@users.noreply.github.com>
2026-05-07 03:31:11 -0700

    feat: enhance ray_color method to support recursive ray tracing and add max_depth parameter

--- include/camera.hpp
+++ include/camera.hpp
@@ -2,13 +2,16 @@
 #define CAMERA_H
 
 #include "hittable.hpp"
+#include "material.hpp"
+#include "ray.hpp"
 
 class camera {
 
 public:
-    double aspect_ratio = 1.0;
-    int image_width = 100;
-    int samples_per_pixels = 10;
+    double aspect_ratio     = 1.0;
+    int image_width         = 100;
+    int samples_per_pixels  = 10;
+    int max_depth           = 10;
 
     void render(const hittable &world) {
 
@@ -22,7 +25,7 @@ public:
                 color pixel_color(0.0, 0.0, 0.0);
                 for(int sample = 0; sample < samples_per_pixels; sample++) {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(std::cout, pixel_samples_scale * pixel_color);
             }
@@ -85,11 +88,22 @@ private:
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
-    color ray_color(const ray& r, const hittable &world) {
-        hit_record rec;
-        if (world.hit(r, interval(0, infinity), rec)) {
-            return 0.5 * (rec.normal + color(1,1,1));
+    color ray_color(const ray& r, int depth, const hittable &world) const {
+
+        if (depth <= 0) {
+            return color(0, 0, 0);
         }
+
+        hit_record rec;
+        if (world.hit(r, interval(0.001, infinity), rec)) {
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered)) {
+                return attenuation * ray_color(scattered, depth - 1, world);
+            }
+            return color(0, 0, 0);
+        }
+
         vec3 unit_direction = unit_vector(r.direction());
         auto a = 0.5*(unit_direction.y() + 1.0);
         return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
