783fcdba0ea19efea89e164b75ca8d77f4872c64
magnimel <65434087+magnimel@users.noreply.github.com>
2026-05-08 01:01:04 -0700

    feat: enhance dielectric material scatter method to include Schlick's approximation for reflectance

--- include/material.hpp
+++ include/material.hpp
@@ -66,7 +66,7 @@ class dielectric: public material {
             bool cannot_refract = ri * sin_theta > 1.0;
             vec3 direction;
 
-            if(cannot_refract) 
+            if(cannot_refract || reflectance(cos_theta, ri) > random_double()) 
                 direction = reflect(unit_direction, rec.normal);
             else
                 direction = refract(unit_direction, rec.normal, ri);
@@ -77,6 +77,13 @@ class dielectric: public material {
 
     private:
         double refraction_index;
+
+        static double reflectance(double cosine, double refraction_index) {
+            // Schlick's approximation
+            auto r0 = (1.0 - refraction_index) / (1.0 + refraction_index);
+            r0 = r0 * r0;
+            return r0 + (1-r0) * std::pow(1-cosine, 5);
+        }
 };
 
 #endif
