07f37c08873722177ffab885ba6e52a241df51eb
magnimel <65434087+magnimel@users.noreply.github.com>
2026-05-10 05:20:19 -0700

    feat: add random_int function for generating random integers within a range

--- include/utility.hpp
+++ include/utility.hpp
@@ -44,6 +44,10 @@ inline double random_double(double min, double max) {
     return min + (max - min) * random_double();
 }
 
+inline int random_int(int min, int max) {
+    return int(random_double(min, max+1));
+}
+
 struct RenderConfig {
     int scene_id = 1;
     int chunk_size = 1;
